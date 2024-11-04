bd371fe53adb6fdb4d013f7e49355c5965079425
kessily <kessilly@gmail.com>
2024-11-04 00:02:50 -0300

    integrando firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+   apiKey: "AIzaSyACscFIs2wc76rxL1wcL7WbOVv4lfJ3HEU",
+   authDomain: "crud-devops-f79e8.firebaseapp.com",
+   databaseURL: "https://crud-devops-f79e8-default-rtdb.firebaseio.com",
+   projectId: "crud-devops-f79e8",
+   storageBucket: "crud-devops-f79e8.firebasestorage.app",
+   messagingSenderId: "392000623162",
+   appId: "1:392000623162:web:0b789d3bd6d7a8e42f5b03",
+   measurementId: "G-GB0KTFSLFD"
+ }
 };
 
 /*
